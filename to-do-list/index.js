0400d029c7dcbc26fad452455dc8c14595f56f78
jiunlee19 <tearofglass@gachon.ac.kr>
2023-08-08 20:28:12 +0900

    DAY 7 : carousel 완성

--- to-do-list/index.js
+++ to-do-list/index.js
@@ -1,29 +1,93 @@
+var cnt=0;
+var taskArray = [];
+
+var Content = document.getElementById("to-do");
+var Priority = document.getElementById("priority");
+
+
 function addTask() {
-    var Content = document.getElementById("to-do");
+    var DON = document.getElementById("DON");
     if(Content.value.length != 0){
+
+        taskArray.push({
+            id: cnt,
+            check: DON.checked , 
+            contents: Content.value, 
+            priority: Priority.value
+        });
+        console.log(taskArray[cnt]);
+
         const taskList = document.getElementById("show");
 
         const taskOption = document.createElement("li");
+        taskOption.id = "options";//필요할까?
+        taskOption.value = cnt;
 
-        const taskPriority = document.createElement("p");
-        var Priority = document.getElementById("priority");
-        taskPriority.innerText = Priority.value;
+        const modify = document.createElement('button');
+        modify.id = modify;
+        modify.innerText = "수정 확인";
 
-        const taskContent = document.createElement("p");
-        taskContent.innerText = Content.value;
-        Content.innerText = '';
-        Content.value = null;
+        const taskSelect = document.createElement("select");
+        taskSelect.id = "taskSelect";
+        const priorityList = ['낮음','보통','높음','아주 높음'];
+        for(const i of priorityList)
+        {
+            const option = document.createElement("option");
+            option.id = 'option';
+            option.text = i; /*taskArray[cnt].priority;*/
+            option.value = i;
+            taskSelect.add(option);
+        }
+        console.log(taskArray[cnt].priority);
+        taskArray[cnt].priority.selected;
+
+        const taskContent = document.createElement("input");
+        taskContent.type = "text";
+        taskContent.value = Content.value;
 
         const checkBox = document.createElement("input");
         checkBox.type = "checkbox";
-
+        checkBox.checked = DON.checked;
 
         //출력돼야 하는 것들 checkbox, text, option
         taskOption.appendChild(checkBox);
         taskOption.appendChild(taskContent);
-        taskOption.appendChild(taskPriority);
+        taskOption.appendChild(taskSelect);
         taskList.appendChild(taskOption);
+        taskOption.appendChild(modify);
+
+        ///초기화
+        DON.checked = false;
+        Content.value = null;
+        Priority.value = "낮음";
+
+        cnt++;
     }
     else { alert("Please type anything"); }
+}
+
+const modifyButton = document.getElementById("modify");
+var Op = document.getElementById('options');
+modifyButton.onclick = infoModify(modifyButton.value);
+
+function infoModify(ii) {
+    console.log(Op[ii].value);
+    /*var modify = document.querySelector("button #modify");
+    taskArray[modify.value].check = DON.checked;
+    taskArray[modify.value].contents = Content.value;
+    taskArray[modify.value].priority = Priority.value;*/
 
 }
+
+/*
+function taskAll() {
+
+}
+
+function taskDone() {
+
+}
+
+function taskNotDone() {
+
+}*/
